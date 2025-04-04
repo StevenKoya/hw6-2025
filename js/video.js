85690549d621940eabae0bb107506d8f4dd19f88
StevenKoya <152226429+StevenKoya@users.noreply.github.com>
2025-04-03 21:22:37 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -3,38 +3,37 @@ var video;
 window.addEventListener("load", function () {
   console.log("Good job opening the window");
 
-  // Initialize the video element
   video = document.getElementById("player1");
   video.autoplay = false;
   video.loop = false;
   video.load();
 
-  // Play Button
+  // Play
   document.getElementById("play").addEventListener("click", function () {
     console.log("Play Video");
     video.play();
     document.getElementById("volume").innerHTML = Math.round(video.volume * 100) + "%";
   });
 
-  // Pause Button
+  //Pause
   document.getElementById("pause").addEventListener("click", function () {
     console.log("Pause Video");
     video.pause();
   });
 
-  // Slow Down Button
+  //Slow 
   document.getElementById("slower").addEventListener("click", function () {
     video.playbackRate *= 0.9;
     console.log("New speed:", video.playbackRate);
   });
 
-  // Speed Up Button
+  //Speed 
   document.getElementById("faster").addEventListener("click", function () {
     video.playbackRate /= 0.9;
     console.log("New speed:", video.playbackRate);
   });
 
-  // Skip Ahead Button
+  // Skip Ahead
   document.getElementById("skip").addEventListener("click", function () {
     if (video.currentTime + 10 < video.duration) {
       video.currentTime += 10;
@@ -44,13 +43,13 @@ window.addEventListener("load", function () {
     console.log("Current time:", video.currentTime);
   });
 
-  // Mute/Unmute Button
+  // Mute/Unmute
   document.getElementById("mute").addEventListener("click", function () {
     video.muted = !video.muted;
     this.innerHTML = video.muted ? "Unmute" : "Mute";
   });
 
-  // Volume Slider
+  // Volume 
   document.getElementById("slider").addEventListener("input", function () {
     video.volume = this.value / 100;
     document.getElementById("volume").innerHTML = this.value + "%";
